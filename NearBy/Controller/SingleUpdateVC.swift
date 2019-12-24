f9dcae51f6bbefc44caaab494d71fbbb9b034f90
Elshakoushy <melshakoushy@hotmail.com>
2019-12-24 21:46:08 +0200

    AllCellSetupFinished

--- NearBy/Controller/SingleUpdateVC.swift
+++ NearBy/Controller/SingleUpdateVC.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class SingleUpdateVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //Constants
+    let placeCellId = "PlaceCell"
+    let noDataCellId = "NoDataCell"
+    let errorCellId = "ErrorCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        setupTableView()
     }
+    
+    func setupTableView() {
+        tableView.register(UINib(nibName: placeCellId, bundle: nil), forCellReuseIdentifier: placeCellId)
+        tableView.register(UINib(nibName: noDataCellId, bundle: nil), forCellReuseIdentifier: noDataCellId)
+        tableView.register(UINib(nibName: errorCellId, bundle: nil), forCellReuseIdentifier: errorCellId)
+    }
+    
+    @IBAction func realtimeBtnPressed(_ sender: Any) {
+    }
+    
+}
 
-
+extension SingleUpdateVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: placeCellId, for: indexPath) as! PlaceCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 115
+    }
 }
 
