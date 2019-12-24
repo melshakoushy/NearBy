b6041520996adfc29f29873a784ad12337765f23
Elshakoushy <melshakoushy@hotmail.com>
2019-12-24 19:07:04 +0200

    project setup

---
+++ NearBy/View/NoDataCell/NoDataCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoDataCell.swift
+//  NearBy
+//
+//  Created by Mahmoud Elshakoushy on 12/24/19.
+//  Copyright © 2019 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import UIKit
+
+class NoDataCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
